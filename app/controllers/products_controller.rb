767cba4111fffa6a06c0ed65c50800ad7c84d405
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-10-13 21:59:10 +0200

    add index and show of CRUD product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :destroy]
   def index
     @products = Product.all
   end
+
+  def show; end
 
   def new
     @product = Product.new # needed to instantiate the form_for
   end
 
+ private
+
+  def set_product
+    @product = Product.find(params[:id])
+    # authorize @product
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :url, :release_date)
+  end
 end
